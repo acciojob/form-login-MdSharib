ddcc961323e3d495897fbcee2ef0939ece9704ad
Md Sharib <109461910+MdSharib@users.noreply.github.com>
2023-07-05 18:12:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	const first =  document.getElementsByTagName("input")[0].value;
+	const last =  document.getElementsByTagName("input")[1].value;
+	alert(`${first} ${last}`);
 }
